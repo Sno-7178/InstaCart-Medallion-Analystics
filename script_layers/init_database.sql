ed3f6c796f329d4853792f4dfc203b6fbc3821a5
Sno-7178 <snoihik.20232079@mnnit.ac.in>
2026-08-01 12:27:32 +0530

    Add files via upload

---
+++ script_layers/init_database.sql
@@ -0,0 +1,11 @@
+-- =============================================================
+--Create Database and Schemas
+-- =============================================================
+-- Purpose:
+-- This script sets up and create three schemas within the 
+-- database 'instacart_medallion' as- 'bronze', 'silver', and 'gold'.
+
+-- create schemas 
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
